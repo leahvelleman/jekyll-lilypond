70608b63b1b47100e30154a53b066d060fb710ee
Leah Velleman <leahvelleman@gmail.com>
2021-01-26 15:12:54 -0500

    Factoring out some boilerplate

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -5,63 +5,76 @@ include Liquid
 RSpec.describe(Jekyll::Lilypond::LilypondTag) do
   let(:site) { make_site }
   let(:context) { make_context(:site => site) }
+  let(:optiontext) {""}
+  let(:content) {""}
+  subject { make_lilypond_tag_object(optiontext,content) }
 
-  before { site.process }
 
   context "initialization" do
+    let (:optiontext) { "a: b c: 'd e' f: \"g h\"" }
     it "stores attribute values" do
-      tag = make_lilypond_tag_object(optiontext = "a: b c: d")
-      expect(tag.attributes["a"]).to eq("b")
-      expect(tag.attributes["c"]).to eq("d")
+      expect(subject.attributes["a"]).to eq("b")
     end
     it "strips quotes from attribute values" do
-      tag = make_lilypond_tag_object(
-        optiontext = "a: 'b c' d: \"e f\"")
-      expect(tag.attributes["a"]).to eq("b c")
-      expect(tag.attributes["d"]).to eq("e f")
+      expect(subject.attributes["c"]).to eq("d e")
+      expect(subject.attributes["f"]).to eq("g h")
     end
   end
 
-  context "rendering" do
-    ["ly", "html"].each do |filetype|
-      it "loads a #{filetype} template" do
-        tag = make_lilypond_tag_object(optiontext = "#{filetype}_template: vacuous_#{filetype}")
-        tag.render(context)
-        expect(tag.send("#{filetype}_template")).to eq(site.layouts["vacuous_#{filetype}"].content)
+  context "unsuccessful template rendering" do
+    it "errors if it gets a .html extension when expecting .ly" do
+      tag = make_lilypond_tag_object(optiontext = "ly_template: vacuous_html")
+      expect { tag.render(context) }.to raise_error(LoadError)
+    end
+    it "errors if it gets a .ly extension when expecting .html" do
+      tag = make_lilypond_tag_object(optiontext = "html_template: vacuous_ly")
+      expect { tag.render(context) }.to raise_error(LoadError)
+    end
+  end
+
+  context "successful template rendering" do
+    before { site.process }
+    before( :each ){ subject.render(context) }
+    context "when used with no arguments" do
+      let( :content ){ "foo" }
+      it 'loads a default lilypond template' do
+        expect(subject.ly_template).to eq(Jekyll::Lilypond::LilypondTag::DEFAULT_LY_TEMPLATE)
       end
-      it "errors if a bad #{filetype} template filename is given" do
-        tag = make_lilypond_tag_object(optiontext = "#{filetype}_template: snarfleblorf")
-        expect { tag.render(context) }.to raise_error(LoadError)
+      it 'loads a default html template' do
+        expect(subject.html_template).to eq(Jekyll::Lilypond::LilypondTag::DEFAULT_MD_TEMPLATE)
       end
-      it "errors if it gets a non-lilypond template instead of a lilypond one" do
-        tag = make_lilypond_tag_object(optiontext = "ly_template: vacuous_html")
-        expect { tag.render(context) }.to raise_error(LoadError)
+    end
+    context "when given a specified template" do
+      let( :optiontext ){ "ly_template: vacuous_ly" }
+      let( :content ){ "foo" }
+      it 'loads a template' do
+        expect(subject.ly_template).to eq(site.layouts["vacuous_ly"].content)
       end
-      it "errors if it gets a non-html template instead of a html one" do
-        tag = make_lilypond_tag_object(optiontext = "html_template: vacuous_ly")
-        expect { tag.render(context) }.to raise_error(LoadError)
+      it 'populates it with content' do
+        expect(subject.ly_source).to eq("foo")
       end
-      it "loads a default #{filetype} template if none is specified" do
-        tag = make_lilypond_tag_object
-        tag.render(context)
-        expect(tag.send("#{filetype}_template")).to eq("{{ content }}")
+    end
+    context "when given a verbal template and variables" do
+      let( :optiontext ){ 
+        "ly_template_text: '{{ a }} {{ content }} {{ c }}'"\
+        "html_template_text: '{{ a }} {{ c }}' a:1 c:3" 
+      }
+      let( :content ){ "2" }
+      it 'accepts the lilypond template' do 
+        expect(subject.ly_template).to eq("{{ a }} {{ content }} {{ c }}")
       end
-      it "accepts a raw #{filetype} template if given one" do
-        tag = make_lilypond_tag_object(optiontext = "#{filetype}_template_text: 'a {{ content }} b'")
-        tag.render(context)
-        expect(tag.send("#{filetype}_template")).to eq("a {{ content }} b")
+      it 'accepts the html template' do
+        expect(subject.html_template).to eq("{{ a }} {{ c }}")
       end
-      it "renders the #{filetype} template" do
-        tag = make_lilypond_tag_object(optiontext = "#{filetype}_template: vacuous_#{filetype}",
-                                       content = "c")
-        tag.render(context)
-        expect(tag.send("#{filetype}_source")).to eq("c")
-      end
-      it "uses attributes when rendering #{filetype} template" do
-        tag = make_lilypond_tag_object(optiontext = "#{filetype}_template: variables_#{filetype} a:1 b:3", content = "2")
-        tag.render(context)
-        expect(tag.send("#{filetype}_source")).to eq("1 2 3")
-      end
+      it 'populates the lilypond template' do expect(subject.ly_source).to eq("1 2 3") end
+      it 'populates the html template'     do expect(subject.html_source).to eq("1 3") end
+    end
+    context "when given a template with liquid filters" do
+      let( :optiontext ){ 
+        "ly_template_text: '{{ a }} {{ content }} {{ c | plus:3 }}' a:1 c:3" 
+      }
+      let( :content ){ "2" }
+      it 'runs them' do expect(subject.ly_source).to eq("1 2 6") end
     end
   end
 end
