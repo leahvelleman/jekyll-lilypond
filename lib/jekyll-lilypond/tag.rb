9a1ebec9041f8ed3ca48b0065bf1c1462858d46d
Leah Velleman <leahvelleman@gmail.com>
2021-02-10 12:24:38 -0500

    Store source and include details separately

--- lib/jekyll-lilypond/tag.rb
+++ lib/jekyll-lilypond/tag.rb
@@ -1,13 +1,15 @@
 module Jekyll
   module Lilypond
     class Tag
-      attr_accessor :attrs, :source_template_name, :include_template_name
+      attr_accessor :attrs, :source_details, :include_details
 
       def initialize(attrs, content)
         @attrs = attrs
         @attrs ["content"] = content
-        @source_template_name = @attrs.delete("source_template")
-        @include_template_name = @attrs.delete("include_template")
+        @source_details = { template_name: @attrs.delete("source_template"), 
+                            template_code: @attrs.delete("source_template_code") }
+        @include_details = { template_name: @attrs.delete("include_template"),
+                             template_code: @attrs.delete("include_template_code") }
       end
     end
   end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -16,9 +16,25 @@ RSpec.describe(Jekyll::Lilypond::Tag) do
       t = described_class.new({"foo" => "bar", "source_template" => "filename.ly"}, "text")
       expect(t.attrs["source_template"]).to eq(nil)
     end
+    it "stores source template addresses in source_details" do
+      t = described_class.new({"foo" => "bar", "source_template" => "filename.ly"}, "text")
+      expect(t.source_details[:template_name]).to eq("filename.ly")
+    end
+    it "stores source template code in source_details" do
+      t = described_class.new({"foo" => "bar", "source_template_code" => "foo"}, "text")
+      expect(t.source_details[:template_code]).to eq("foo")
+    end
     it "stores include template addresses separately" do
       t = described_class.new({"foo" => "bar", "include_template" => "filename.ly"}, "text")
       expect(t.attrs["include_template"]).to eq(nil)
+    end
+    it "stores include template addresses in source_details" do
+      t = described_class.new({"foo" => "bar", "include_template" => "filename.ly"}, "text")
+      expect(t.include_details[:template_name]).to eq("filename.ly")
+    end
+    it "stores include template code in include_details" do
+      t = described_class.new({"foo" => "bar", "include_template_code" => "foo"}, "text")
+      expect(t.include_details[:template_code]).to eq("foo")
     end
     it "copes with missing text" do
       expect { described_class.new({"foo" => "bar"}, "") }.to_not raise_error()
