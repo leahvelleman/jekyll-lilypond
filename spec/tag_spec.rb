31a78fdf3bbf2310d5a7dea09d7df45813e49019
Leah Velleman <leahvelleman@gmail.com>
2021-03-19 14:36:31 -0400

    Tag scores template names together, and template code together

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -16,28 +16,33 @@ RSpec.describe(Jekyll::Lilypond::Tag) do
       t = described_class.new({"foo" => "bar", "source_template" => "filename.ly"}, "text")
       expect(t.attrs["source_template"]).to eq(nil)
     end
-    it "stores source template addresses in source_details" do
+    it "stores source template addresses in template_names" do
       t = described_class.new({"foo" => "bar", "source_template" => "filename.ly"}, "text")
-      expect(t.source_details[:template_name]).to eq("filename.ly")
+      expect(t.template_names[:source]).to eq("filename.ly")
     end
-    it "stores source template code in source_details" do
+    it "stores source template code in template_code" do
       t = described_class.new({"foo" => "bar", "source_template_code" => "foo"}, "text")
-      expect(t.source_details[:template_code]).to eq("foo")
+      expect(t.template_code[:source]).to eq("foo")
     end
     it "stores include template addresses separately" do
       t = described_class.new({"foo" => "bar", "include_template" => "filename.ly"}, "text")
       expect(t.attrs["include_template"]).to eq(nil)
     end
-    it "stores include template addresses in source_details" do
+    it "stores include template addresses in template_names" do
       t = described_class.new({"foo" => "bar", "include_template" => "filename.ly"}, "text")
-      expect(t.include_details[:template_name]).to eq("filename.ly")
+      expect(t.template_names[:include]).to eq("filename.ly")
     end
-    it "stores include template code in include_details" do
+    it "stores include template code in template_code" do
       t = described_class.new({"foo" => "bar", "include_template_code" => "foo"}, "text")
-      expect(t.include_details[:template_code]).to eq("foo")
+      expect(t.template_code[:include]).to eq("foo")
     end
     it "copes with missing text" do
       expect { described_class.new({"foo" => "bar"}, "") }.to_not raise_error()
+    end
+    it "creates template_names and template_code even when no values are provided" do
+      t = described_class.new({"foo" => "bar"}, "")
+      expect(t.template_names).to eq({source: nil, include: nil})
+      expect(t.template_code).to eq({source: nil, include: nil})
     end
     it "copes with empty attrs" do
       expect { described_class.new({}, "text") }.to_not raise_error()
